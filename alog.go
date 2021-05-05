8faa10d450322216b046c0a3b61384cbe967da52
George Blake <georgeblake6@hotmail.com>
2021-05-04 19:55:30 -0500

    added explanation of defer

--- alog.go
+++ alog.go
@@ -55,7 +55,7 @@ func (al Alog) formatMessage(msg string) string {
 
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
 	al.m.Lock()
-	defer al.m.Unlock()
+	defer al.m.Unlock() //allows to unlock mutex after a panic
 	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
 	
 	if err != nil {
